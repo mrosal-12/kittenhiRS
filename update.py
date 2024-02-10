45df5fc24ea81eac65095cd0dff269f6c5b74919
mrosal-12 <130192407+mrosal-12@users.noreply.github.com>
2024-02-10 13:22:48 -0800

    lol

---
+++ update.py
@@ -0,0 +1,3 @@
+# So i need this to read the configs, see the event key, then do the following:
+#   1) Check tba for the match schedule, then update the csv file if I need to
+#   2) See if a storage file already exists, if it doesn't, make it
